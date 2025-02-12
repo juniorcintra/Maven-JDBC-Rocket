a45d13500f076af801a32ffb976f2d6ba9538967
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-12 00:07:14 -0300

    instalando dependencias

---
+++ src/main/java/ClassMaven.java
@@ -0,0 +1,5 @@
+public class ClassMaven {
+    public static void main( String[] args){
+        System.out.println("Projeto Maven");
+    }
+}
---
+++ src/main/java/PowerClass.java
@@ -0,0 +1,5 @@
+public class PowerClass {
+    public static void main( String[] args){
+        System.out.println("Projeto Maven");
+    }
+}
